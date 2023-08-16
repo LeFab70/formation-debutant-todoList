c17a14564ace2f70c9b409adeb0e8e1bcca987bf
LeFab70 <kouonang2002@gmail.com>
2023-08-16 15:36:24 -0400

    creation des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { OneTodoComponent } from './one-todo/one-todo.component';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoListComponent,
+    OneTodoComponent,
+    CreateTodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/one-todo/one-todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-todo',
+  templateUrl: './one-todo.component.html',
+  styleUrls: ['./one-todo.component.css']
+})
+export class OneTodoComponent {
+
+}
